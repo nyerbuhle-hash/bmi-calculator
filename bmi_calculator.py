cbf8ce8844dbb32db8daa086533c2ed86cfd8c5e
nyerbuhle-hash <nyerbuhle@gmail.com>
2025-12-07 00:45:48 +0000

    Create bmi_calculator.py and add BMI calculator script

---
+++ bmi_calculator.py
@@ -0,0 +1,8 @@
+height = float(input("Enter your height in metres: "))
+weight = float(input("Enter your weight in kilograms: "))
+
+bmi = weight / (height **2)
+
+print("Your BMI is:", round(bmi, 2)
+
+      
